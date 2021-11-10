3b5c061d25043227729a31215a172c70d147d508
Brandon Rotkel <brotkel@gmail.com>
2021-11-09 19:08:12 -0800

    Added postinstall instructions.

---
+++ postinstall.js
@@ -0,0 +1,5 @@
+console.log(`Pattern Lab Node Plugin - "patternlab-plugin-darkmode" installed.
+Use the CLI to install this: patternlab install --plugins patternlab-plugin-darkmode
+Configure or disable this plugin inside your patternlab-config.json file.
+Set your className and element to attach the class to on the plugins['patternlab-plugin-darkmode'].options object.
+`);
